cfb3c9ae4713034ab621aa554ddd8c32a1892aa8
Tobias Gleiter <gleiter.tobias@outlook.de>
2023-10-30 20:24:57 +0100

    added cors config file

--- src/index.ts
+++ src/index.ts
@@ -3,26 +3,21 @@ import 'dotenv/config';
 import cors from 'cors';
 
 import express, { Express, Request, Response } from 'express';
+import { options } from './cors.config';
 
 const app: Express = express();
 const port = process.env.PORT;
  
-// Add a list of allowed origins.
-// If you have more origins you would like to add, you can add them to the array below.
-const allowedOrigins = ['http://localhost:3000'];
 
-const options: cors.CorsOptions = {
-  origin: allowedOrigins
-};
 
 app.use(cors(options));
 app.use(express.json());
  
 app.get('/products/:id', function (req: Request, res: Response, next) {
-  res.json({msg: 'This is CORS-enabled for all origins!'})
+  res.json({msg: 'This is CORS-enabled for localhost:3000!'})
 })
  
 app.listen(port, function () {
-  console.log('CORS-enabled web server listening on port 80')
+  console.log(`CORS-enabled web server listening on port ${port}`)
 })
 
