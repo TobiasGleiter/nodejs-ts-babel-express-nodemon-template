f6862c5341098cec53a456d1cf830b10026e0be7
Tobias Gleiter <gleiter.tobias@outlook.de>
2023-10-30 20:19:13 +0100

    added cors and express

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,28 @@
 "use strict";
 import 'dotenv/config';
+import cors from 'cors';
 
-const sayHello = (subject: string): void => {
-    console.log('Hello ' + subject);
-  };
-  
-  sayHello('TypeScript');
+import express, { Express, Request, Response } from 'express';
 
-console.log(process.env.MY_SECRET);
+const app: Express = express();
+const port = process.env.PORT;
+ 
+// Add a list of allowed origins.
+// If you have more origins you would like to add, you can add them to the array below.
+const allowedOrigins = ['http://localhost:3000'];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
+
+app.use(cors(options));
+app.use(express.json());
+ 
+app.get('/products/:id', function (req: Request, res: Response, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+ 
+app.listen(port, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
+
