9b4345175c68830e4ee78e1b5080982b394f9f51
Tobias Gleiter <gleiter.tobias@outlook.de>
2023-10-30 20:28:45 +0100

    added vscode settings

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,20 @@
 "use strict";
-import 'dotenv/config';
-import cors from 'cors';
+import cors from "cors";
+import "dotenv/config";
 
-import express, { Express, Request, Response } from 'express';
-import { options } from './cors.config';
+import express, { Express, Request, Response } from "express";
+import { options } from "./cors.config";
 
 const app: Express = express();
 const port = process.env.PORT;
- 
-
 
 app.use(cors(options));
 app.use(express.json());
- 
-app.get('/products/:id', function (req: Request, res: Response, next) {
-  res.json({msg: 'This is CORS-enabled for localhost:3000!'})
-})
- 
-app.listen(port, function () {
-  console.log(`CORS-enabled web server listening on port ${port}`)
-})
 
+app.get("/products/:id", function (req: Request, res: Response, next) {
+  res.json({ msg: "This is CORS-enabled for localhost:3000!" });
+});
+
+app.listen(port, function () {
+  console.log(`CORS-enabled web server listening on port ${port}`);
+});
